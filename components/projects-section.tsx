cc418a7e00a03205e5625e8c54c7c8808ce2b0b1
Noureddine <121136079+NoureddineBou@users.noreply.github.com>
2025-04-26 19:20:44 +0100

    changes

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -37,7 +37,7 @@ const projects = [
       "Font Awesome",
       "Google Fonts",
     ],
-    liveUrl: "athar-baqi.vercel.app", // if you have a live version
+    liveUrl: "https://athar-baqi.vercel.app", // if you have a live version
     githubUrl: "https://github.com/nxr-deen/athar-baqi",
   },
 
